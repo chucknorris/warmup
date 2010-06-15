a816db3c7adf959084f944b27f5691dd537a5d1b
Dru Sellers <dru@drusellers.com>
2010-06-15 06:15:43 -0500

    more legal and config file notes

--- warmup/settings/IgnoredFileTypeCollection.cs
+++ warmup/settings/IgnoredFileTypeCollection.cs
@@ -1,27 +1,36 @@
-﻿
-
-
-using System.Collections.Generic;
-
+﻿// Copyright 2007-2010 The Apache Software Foundation.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace warmup.settings
 {
-
-    using System;
     using System.Configuration;
-    public class IgnoredFileTypeCollection : ConfigurationElementCollection//, IEnumerable<IgnoredFileType>
+
+    public class IgnoredFileTypeCollection : ConfigurationElementCollection //, IEnumerable<IgnoredFileType>
     {
         protected override ConfigurationElement CreateNewElement()
         {
             return new IgnoredFileType();
         }
+
         protected override object GetElementKey(ConfigurationElement element)
         {
-            return ((IgnoredFileType)element).Extension;
+            return ((IgnoredFileType) element).Extension;
         }
+
         public void Clear()
         {
             BaseClear();
         }
+
         public int IndexOf(IgnoredFileType ignoredFileType)
         {
             return BaseIndexOf(ignoredFileType);
@@ -32,22 +41,14 @@ namespace warmup.settings
             BaseAdd(ignoredFileType);
         }
     }
+
     public class IgnoredFileType : ConfigurationElement
     {
         [ConfigurationProperty("ext", IsRequired = true, IsKey = true)]
         public string Extension
         {
-            get
-            {
-                return (string)this["ext"];
-            }
-            set
-            {
-                this["ext"] = value;
-            }
+            get { return (string) this["ext"]; }
+            set { this["ext"] = value; }
         }
-
     }
-
-
 }
--- warmup/settings/WarmupConfiguration.cs
+++ warmup/settings/WarmupConfiguration.cs
@@ -1,9 +1,21 @@
+// Copyright 2007-2010 The Apache Software Foundation.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
+// this file except in compliance with the License. You may obtain a copy of the 
+// License at 
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software distributed 
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+// specific language governing permissions and limitations under the License.
 namespace warmup.settings
 {
     using System.Configuration;
 
     /// <summary>
-    /// Configuration Handler for Warmup
+    ///   Configuration Handler for Warmup
     /// </summary>
     public sealed class WarmupConfiguration : ConfigurationSection
     {
@@ -11,7 +23,7 @@ namespace warmup.settings
             ConfigurationManager.GetSection("warmup") as WarmupConfiguration;
 
         /// <summary>
-        /// Settings section
+        ///   Settings section
         /// </summary>
         public static WarmupConfiguration settings
         {
@@ -19,39 +31,41 @@ namespace warmup.settings
         }
 
         /// <summary>
-        /// The top level location for warmup templates
+        ///   The top level location for warmup templates
         /// </summary>
         [ConfigurationProperty("sourceControlWarmupLocation", IsRequired = true)]
         public string SourceControlWarmupLocation
         {
-            get { return (string)this["sourceControlWarmupLocation"]; }
+            get { return (string) this["sourceControlWarmupLocation"]; }
         }
+
         /// <summary>
-        /// The token to replace in the warmup templates. Not required, default value is "__NAME__"
+        ///   The token to replace in the warmup templates. Not required, default value is "__NAME__"
         /// </summary>
         [ConfigurationProperty("replacementToken", IsRequired = false, DefaultValue = "__NAME__")]
         public string ReplacementToken
         {
-            get { return (string)this["replacementToken"]; }
+            get { return (string) this["replacementToken"]; }
         }
 
         /// <summary>
-        /// What type of source control are we using?
+        ///   What type of source control are we using?
         /// </summary>
-        [ConfigurationProperty("sourceControlType", IsRequired = false, DefaultValue="svn")]
+        [ConfigurationProperty("sourceControlType", IsRequired = false, DefaultValue = "svn")]
         public string SourceControl
         {
-            get { return (string)this["sourceControlType"]; }
+            get { return (string) this["sourceControlType"]; }
         }
 
         public SourceControlType SourceControlType
         {
-            get { 
+            get
+            {
                 if (SourceControl.Contains("git"))
                 {
                     return SourceControlType.Git;
                 }
-                if (SourceControl.Contains("File"))
+                if (SourceControl.ToLower().Contains("file"))
                 {
                     return SourceControlType.FileSystem;
                 }
@@ -59,14 +73,11 @@ namespace warmup.settings
                 return SourceControlType.Subversion;
             }
         }
+
         [ConfigurationProperty("ignoredExtensions", IsDefaultCollection = false)]
         public IgnoredFileTypeCollection IgnoredFileTypeCollection
         {
-            get
-            {
-                return (IgnoredFileTypeCollection)base["ignoredExtensions"];
-            }
+            get { return (IgnoredFileTypeCollection) base["ignoredExtensions"]; }
         }
-
     }
 }
